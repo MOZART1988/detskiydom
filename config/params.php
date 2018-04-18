8b1eac8cbb3e03b8a0e7e46372a0a931e07abfbd
ivan <ivan@rocketfirm.com>
2018-04-18 17:20:26 +0600

    превью behavior

--- config/params.php
+++ config/params.php
@@ -2,11 +2,4 @@
 return [
     'adminEmail' => 'admin@sitename.kz',
     'fromEmail' => 'support@website.kz',
-    'previews' => [
-        'pages' => [
-            [398, 277],
-            [310, 216]
-        ]
-    ]
-
 ];
