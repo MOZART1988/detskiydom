eff2971aa907c8acaa246cbc88a6a2dac2a48ab3
ivan <ivan@rocketfirm.com>
2018-04-24 15:53:24 +0600

    слайдер новый

--- components/widgets/views/sliderWidget.php
+++ components/widgets/views/sliderWidget.php
@@ -32,26 +32,6 @@
                             </div>
                         </div>
                     <?php endforeach; ?>
-
-                    <div class="swiper-slide">
-                        <div class="top-slider--text">
-                            <div class="slider-text">
-                                <h3 class="orange-title">О фонде</h3>
-                                <p>Общественный фонд «Помощи детям и подросткам с синдромом Дауна КYН БАЛА» существует с 14 августа 2015 года. Фонд организован при участии семей, воспитывающих детей и подростков с синдромом Дауна (СД) и является на сегодняшний день
-                                    первым и единственным фондом на территории г. Алматы, представляющим интересы людей с СД. Наш фонд объединяет и представляет интересы более 100 семей на территории Алматы и Алматинской области, в которых живут и воспитываются «солнечные»
-                                    дети.</p>
-                            </div>
-                            <div class="read-more">
-                                <a href="">читать все</a>
-                            </div>
-                        </div>
-                        <div class="top-slider-wrapper">
-                            <div class="top-slider--block">
-                                <img src="images/slide1.jpg" alt=""> </div>
-                            <img src="images/slider-bottom.png" alt="" class="slider-bottom">
-                            <!-- If we need pagination -->
-                        </div>
-                    </div>
                 </div>
                 <div class="swiper-pagination"></div>
             </div>
