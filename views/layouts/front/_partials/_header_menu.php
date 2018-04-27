808be6dc9f324ca0bf623cac9bc42978771f46b3
ivan <ivan@rocketfirm.com>
2018-04-27 10:05:53 +0600

    мобильная версия сайта

--- views/layouts/front/_partials/_header_menu.php
+++ views/layouts/front/_partials/_header_menu.php
@@ -27,7 +27,24 @@
     </ul>
 
     <div class="menu-dropdown">
-        <a href="#" class="toggle-dropdown">Главная</a>
+        <?php if (\Yii::$app->request->url === '/') : ?>
+            <a href="#" class="toggle-dropdown">Главная</a>
+        <?php elseif (\Yii::$app->controller->module->id === 'content' && \Yii::$app->controller->action->id === 'view' && (\Yii::$app->request->get('id') === '1')) :?>
+            <a href="#" class="toggle-dropdown">О фонде</a>
+        <?php elseif(\Yii::$app->controller->module->id === 'pages' && (\Yii::$app->controller->action->id === 'news' ||
+            \Yii::$app->controller->action->id === 'news-view')) :?>
+            <a href="#" class="toggle-dropdown">Пресс-центр</a>
+        <?php elseif (\Yii::$app->controller->module->id === 'pages' && (\Yii::$app->controller->action->id === 'programms' ||
+            \Yii::$app->controller->action->id === 'programm-view')) :?>
+            <a href="#" class="toggle-dropdown">Программы фонда</a>
+        <?php elseif(\Yii::$app->controller->module->id === 'pages' && (\Yii::$app->controller->action->id === 'info' ||
+            \Yii::$app->controller->action->id === 'info-view')) : ?>
+            <a href="#" class="toggle-dropdown">Полезная информация</a>
+        <?php elseif(\Yii::$app->controller->module->id === 'histories') : ?>
+            <a href="#" class="toggle-dropdown">Живые истории</a>
+        <?php elseif(\Yii::$app->controller->module->id === 'content' && \Yii::$app->controller->action->id === 'contact'): ?>
+            <a href="#" class="toggle-dropdown">Контакты</a>
+        <?php endif; ?>
         <div class="menu-dropdown--list">
             <ul>
                 <li class="<?=(\Yii::$app->request->url === '/') ? 'active' : ''?>">
