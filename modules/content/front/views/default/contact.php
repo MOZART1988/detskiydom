bb510b4be2fd8f5818f376564756e952fc7361ed
ivan <ivan@rocketfirm.com>
2018-06-18 11:26:52 +0600

    мультязычность переводы

--- modules/content/front/views/default/contact.php
+++ modules/content/front/views/default/contact.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="content">
-        <h1 class="yellow-title">Контакты</h1>
+        <h1 class="yellow-title"><?=\Yii::t('front', 'Контакты')?></h1>
         <div class="map-block">
             <div id="map"></div>
             <script>
@@ -37,22 +37,22 @@
         </div>
         <div class="contacts-list">
             <div class="contact-item">
-                <span>Адрес:</span>
-                <p>г. Алматы, ул. Кабдолова, 26, здание МЦ "Авиценна", вход слева</p>
+                <span><?=\Yii::t('front', 'Адрес:')?></span>
+                <p><?=\Yii::t('front', 'г. Алматы, ул. Кабдолова, 26, здание МЦ "Авиценна", вход слеваг. Алматы, ул. Кабдолова, 26, здание МЦ "Авиценна", вход слева')?></p>
             </div>
             <div class="contact-item">
-                <span>Телефоны:</span>
+                <span><?=\Yii::t('front', 'Телефоны:')?></span>
                 <p>+7(701) 794-12-95</p>
                 <p>+7(777) 591-00-77</p>
                 <p>+7(777) 888-80-98</p>
             </div>
             <div class="contact-item">
-                <span>Электронная почта:</span>
+                <span><?=\Yii::t('front', 'Электронная почта:')?></span>
                 <p>e-mail:kunbala.fund@gmail.com</p>
             </div>
         </div>
         <div class="contact-form">
-            <div class="form-text"> Обратная связь: </div>
+            <div class="form-text"> <?=\Yii::t('front', 'Обратная связь:')?> </div>
             <?php $form = \yii\widgets\ActiveForm::begin(['id' => 'sidebar-contact-widget', 'action' => \yii\helpers\Url::to(['/pages/default/send-form/'])]); ?>
                 <?= $form->field($model, 'name')->textInput(['placeholder' => \Yii::t('front', 'Имя')])->label(false) ?>
                 <?= $form->field($model, 'email')->textInput(['placeholder' => \Yii::t('front', 'Электронная почта')])->label(false) ?>
@@ -60,7 +60,7 @@
                 'placeholder' => \Yii::t('front', 'Ваши предложения'),
                 'rows' => 7
                 ])->label(false) ?>
-                <button type="submit" class="btn btn--form">ОТПРАВИТЬ</button>
+                <button type="submit" class="btn btn--form"><?=\Yii::t('front', 'ОТПРАВИТЬ')?></button>
             <?php \yii\widgets\ActiveForm::end(); ?>
         </div>
     </div>
